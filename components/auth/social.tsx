d62c1ab679fd017f7cec5f7069b251ea4b4f6a1a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-18 11:57:52 +0200

    Created Social component

---
+++ components/auth/social.tsx
@@ -0,0 +1,18 @@
+
+const Social = () => {
+  return (
+    <div className="btn-list text-center">
+          <button aria-label="button" type="button" className="ti-btn ti-btn-icon ti-btn-light me-[0.365rem]">
+              <i className="ri-facebook-line font-bold text-dark opacity-[0.7]"></i>
+          </button>
+          <button aria-label="button" type="button" className="ti-btn ti-btn-icon ti-btn-light me-[0.365rem]">
+              <i className="ri-google-line font-bold text-dark opacity-[0.7]"></i>
+          </button>
+          <button aria-label="button" type="button" className="ti-btn ti-btn-icon ti-btn-light">
+              <i className="ri-twitter-line font-bold text-dark opacity-[0.7]"></i>
+          </button>
+      </div>
+  )
+}
+
+export default Social
