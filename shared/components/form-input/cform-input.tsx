1a783391b17ecb019d7c513ae69b92f6a93fa0cb
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-19 09:16:15 +0200

    Created Select component

--- shared/components/form-input/cform-input.tsx
+++ shared/components/form-input/cform-input.tsx
@@ -1,33 +1,10 @@
-import { FC, InputHTMLAttributes,RefCallback } from 'react'
+import { FC } from 'react'
 import CFormError from '@/shared/components/form-error/cform-error'
+import { InputProps } from '../props';
 
 
-interface InputProps {
-    onChange: (event:any) => unknown;
-    onBlur: (event:any) => unknown;
-    ref: RefCallback<HTMLInputElement>;
-    name?: string;
-    min?: string | number;
-    max?: string | number;
-    maxLength?: number;
-    minLength?: number;
-    pattern?: string;
-    required?: boolean;
-    disabled?: boolean;
-}
 
-interface Props extends InputHTMLAttributes<HTMLInputElement>{
-    name:string,
-    label:string,
-    labelPosition?:'top'|'side'
-    required?:boolean,
-    className?:string,
-    inputProps?:InputProps,
-    error?:string
-}
-
-
-const CFormInput:FC<Props>  = ({name,label,labelPosition='side',required=false,className='',inputProps,error,...rest}) => {
+const CFormInput:FC<InputProps>  = ({name,label,labelPosition='side',required=false,className='',props,error,...rest}) => {
     const requiredCss = required? 'required' : '';
     const labelHtml = (
         <label htmlFor={name} className={`form-label text-default ${requiredCss}`}>
@@ -35,45 +12,38 @@ const CFormInput:FC<Props>  = ({name,label,labelPosition='side',required=false,c
         </label>
     )
 
+    const inputHtml = (
+        <input 
+            id={name}  
+            name={name} 
+            size={30}
+            className={`form-control form-control-lg ${className} `}
+            aria-invalid={error ? "true" : "false"}
+            {...(props ?? {})}
+            {...rest}/>
+    );
+
+    const errorHtml = error ? (<CFormError error={error} />) : null;
+
     const innerHtml = labelPosition==='side'? (
         <>
             {labelHtml}
             <div className="col-lg-8 fv-row">
-                <input 
-                  id={name}  
-                  name={name} 
-                  size={30}
-                  className={`form-control form-control-lg form-control-solid mb-3 mb-lg-0 ${className} `}
-                  aria-invalid={error ? "true" : "false"}
-                  {...(inputProps ?? {})}
-                  {...rest}/>
-
-                {
-                    error ? (<CFormError error={error} />) : null
-                }
+                {inputHtml}
+                {errorHtml}
             </div>
         </>
     ) :(
         <div className="fv-row mb-0">
             {labelHtml}
-            <input 
-                  id={name}  
-                  name={name} 
-                  size={30}
-                  className={`form-control form-control-lg form-control-solid  ${className} `}
-                  aria-invalid={error ? "true" : "false"}
-                  {...(inputProps ?? {})}
-                  {...rest}/>
-
-                {
-                    error ? (<CFormError error={error} />) : null
-                }
+            {inputHtml}
+            {errorHtml}
         </div>
     );
 
 
     return (
-        <section className="row mb-6">
+        <section className="xl:col-span-12 col-span-12">
             {innerHtml}
         </section>
     )
---
+++ shared/components/form-select/cform-select.tsx
@@ -0,0 +1,37 @@
+import {FC} from 'react'
+import CFormError from '@/shared/components/form-error/cform-error'
+import { SelectProps } from '../props'
+
+
+const CFormSelect:FC<SelectProps> = ({label,name,options,required=false,inputProps,error,...rest}) => {
+    const labelHtml = required? (
+        <label htmlFor={name} className="col-lg-4 col-form-label fw-bold fs-6">
+            <span className='required'>{label}</span>
+        </label>
+    ):(
+        
+        <label htmlFor={name} className="col-lg-4 col-form-label fw-bold fs-6">
+            {label}
+        </label>
+    )
+
+    return (
+        <section className="row mb-6">
+            {labelHtml}
+            <div className='col-lg-8 fv-row'>
+                <select className='form-select form-select-solid form-select-lg fw-bold' 
+                    {...(inputProps ?? {})}
+                    {...rest}>
+                    {options.map((option) =>(
+                        <option key={option.value} value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+                {
+                    error ? (<CFormError error={error} />) : null
+                }
+            </div>
+        </section>
+    )
+}
+
+export default CFormSelect
