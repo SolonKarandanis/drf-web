aef68e413aa2ebab8954a822e6b126795ef70b8e
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-19 15:37:45 +0200

    Refactoring Register Form

--- components/auth/forms/register-form.tsx
+++ components/auth/forms/register-form.tsx
@@ -35,29 +35,29 @@ const RegisterForm = () => {
             <div className="xl:col-span-12 col-span-12">
                 <label htmlFor="firstname" className="form-label text-default">First Name</label>
                 <input type="text" className="form-control form-control-lg w-full !rounded-md"
-                    id="firstname" placeholder="first name"/>
+                    id="firstname" placeholder="first name" {...register("firstName")}/>
             </div>
             <div className="xl:col-span-12 col-span-12">
                 <label htmlFor="lastname" className="form-label text-default">Last Name</label>
                 <input type="text" className="form-control form-control-lg w-full !rounded-md"
-                    id="lastname" placeholder="last name"/>
+                    id="lastname" placeholder="last name" {...register("lastName")}/>
             </div>
             <div className="xl:col-span-12 col-span-12">
                 <label htmlFor="email" className="form-label text-default">Email</label>
                 <input type="email" className="form-control form-control-lg w-full !rounded-md"
-                    id="email" placeholder="Email"/>
+                    id="email" placeholder="Email" {...register("email")}/>
             </div>
             <div className="xl:col-span-12 col-span-12">
                 <label htmlFor="username" className="form-label text-default">Username</label>
                 <input type="text" className="form-control form-control-lg w-full !rounded-md"
-                    id="username" placeholder="Username" autoComplete="username"/>
+                    id="username" placeholder="Username" autoComplete="username" {...register("username")}/>
             </div>
             <div className="xl:col-span-12 col-span-12">
                 <label htmlFor="password" className="form-label text-default">Password</label>
                 <div className="input-group">
-                    <input type="password"
+                    <input type={(showPassword) ? 'text' : "password"}
                         className="form-control form-control-lg !rounded-e-none"
-                        id="password" placeholder="password" autoComplete="password"/>
+                        id="password" placeholder="password" autoComplete="password" {...register("password")}/>
                     <button onClick={()=>setShowPassword(!showPassword)}  aria-label="button" 
                         className="ti-btn ti-btn-light !rounded-s-none !mb-0" type="button" id="button-addon2">
                         <i className={`${showPassword ? 'ri-eye-line' : 'ri-eye-off-line'} align-middle`}></i>
@@ -68,23 +68,24 @@ const RegisterForm = () => {
                 <label htmlFor="confirmpassword" className="form-label text-default">Confirm
                     Password</label>
                 <div className="input-group">
-                    <input type="password"
+                    <input type={(showConfirmPassword) ? 'text' : "password"}
                         className="form-control form-control-lg !rounded-e-none"
-                        id="confirmpassword" placeholder="confirm password" autoComplete="new-password"/>
+                        id="confirmpassword" placeholder="confirm password" autoComplete="new-password" {...register("confirmPassword")}/>
                     <button onClick={()=>setConfirmShowPassword(!showConfirmPassword)}  aria-label="button" 
                         className="ti-btn ti-btn-light !rounded-s-none !mb-0" type="button" id="button-addon21">
-                        <i className={`${showPassword ? 'ri-eye-line' : 'ri-eye-off-line'} align-middle`}></i>
+                        <i className={`${showConfirmPassword ? 'ri-eye-line' : 'ri-eye-off-line'} align-middle`}></i>
                     </button>
                 </div>
-                <div className="mt-4">
-                    <div className="form-check !flex !ps-0">
-                        <input className="form-check-input me-1" type="checkbox" value="" id="defaultCheck1"/>
-                        <label className="ps-2 form-check-label text-[#8c9097] dark:text-white/50 font-normal block" htmlFor="defaultCheck1">
-                            By creating a account you agree to our <Link href="/components/pages/terms&conditions/"
-                            className="text-success"><u>Terms &amp; Conditions</u></Link> and <Link href="#!"
-                            className="text-success"><u>Privacy Policy</u></Link>
-                        </label>
-                    </div>
+                
+            </div>
+            <div className="xl:col-span-12 col-span-12">
+                <div className="form-check !flex !ps-0">
+                    <input className="form-check-input me-1" type="checkbox" value="" id="defaultCheck1"/>
+                    <label className="ps-2 form-check-label text-[#8c9097] dark:text-white/50 font-normal block" htmlFor="defaultCheck1">
+                        By creating a account you agree to our <Link href="/components/pages/terms&conditions/"
+                        className="text-success"><u>Terms &amp; Conditions</u></Link> and <Link href="#!"
+                        className="text-success"><u>Privacy Policy</u></Link>
+                    </label>
                 </div>
             </div>
             <div className="xl:col-span-12 col-span-12 grid mt-2">
