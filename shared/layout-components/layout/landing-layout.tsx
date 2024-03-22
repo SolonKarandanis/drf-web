0e175870d6ec3aaf0d5ffb8642e73455b1faccd8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-22 08:55:48 +0200

    Created Landinglayout

---
+++ shared/layout-components/layout/landing-layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, useEffect } from 'react'
+import { Provider } from 'react-redux'
+import {makeStore} from "@/shared/redux/store"
+
+const Landinglayout = ({children}:{ children: ReactNode}) => {
+    useEffect(() => {
+		import("preline");
+
+	}, []);
+
+    return (
+        <>
+            <Provider store={makeStore()}>
+                {children}
+            <div id="responsive-overlay"></div>
+            </Provider>
+        </>
+    )
+}
+
+export default Landinglayout
