be5d152978edd3ea86f7c6ff7750991c19d17bea
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-23 11:22:23 +0200

    Implementing Header component

--- shared/layout-components/header/Header.tsx
+++ shared/layout-components/header/Header.tsx
@@ -1,6 +1,140 @@
-import React from 'react'
+'use client';
+
+import { useEffect, useState} from 'react'
 
 const Header = () => {
+  const data=  <span className="font-[600] py-[0.25rem] px-[0.45rem] rounded-[0.25rem] bg-pink/10 text-pink text-[0.625rem]">Free shipping</span>
+  const cartProduct = [
+    {
+      id: 1,
+      src: "/assets/images/ecommerce/jpg/1.jpg",
+      name: 'SomeThing Phone',
+      price: '$1,299.00',
+      color: 'Metallic Blue',
+      text: '6gb Ram',
+      class: '',
+    },
+    {
+      id: 2,
+      src: "/assets/images/ecommerce/jpg/3.jpg",
+      name: 'Stop Watch',
+      price: '$179.29',
+      color: 'Analog',
+      text: data,
+      class: '',
+    },
+    {
+      id: 3,
+      src: "/assets/images/ecommerce/jpg/5.jpg",
+      name: 'Photo Frame',
+      price: '$29.00',
+      color: 'Decorative',
+      text: '',
+      class: '',
+    },
+    {
+      id: 4,
+      src: "/assets/images/ecommerce/jpg/4.jpg",
+      name: 'Kikon Camera',
+      price: '$4,999.00',
+      color: 'Black',
+      text: '50MM',
+      class: '',
+    },
+    {
+      id: 5,
+      src: "/assets/images/ecommerce/jpg/6.jpg",
+      name: 'Canvas Shoes',
+      price: '$129.00',
+      color: 'Gray',
+      text: 'Sports',
+      class: 'border-b-0',
+    },
+  ];
+
+  const [cartItems, setCartItems] = useState([...cartProduct]);
+  const [cartItemCount, setCartItemCount] = useState(cartProduct.length);
+
+  const handleRemove = (itemId:number) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== itemId);
+    setCartItems(updatedCartItems);
+    setCartItemCount(updatedCartItems.length);
+  };
+
+   //Notifications
+
+   const span1 = <span className="text-warning">ID: #1116773</span>
+   const span2 = <span className="text-success">ID: 7731116</span>
+ 
+   const notifydata = [
+     { id: 1, class: "Your Order Has Been Shipped", data: "Order No: 123456 Has Shipped To Your Delivery Address", icon: "gift", class2: "", color: "primary" },
+     { id: 2, class: "Discount Available", data: "Discount Available On Selected Products", icon: "discount-2", class2: "", color: "secondary" },
+     { id: 3, class: "Account Has Been Verified", data: "Your Account Has Been Verified Sucessfully", icon: "user-check", class2: "", color: "pink" },
+     { id: 4, class: "Order Placed", data: "Order Placed Successfully", icon: "circle-check", class2: span1, color: "warning" },
+     { id: 5, class: "Order Delayed", data: "Order Delayed Unfortunately", icon: "clock", class2: span2, color: "success" },
+   ]
+
+  const [notifications, setNotifications] = useState([...notifydata]);
+
+  const handleNotificationClose = (index:number) => {
+    const updatedNotifications = [...notifications];
+    updatedNotifications.splice(index, 1);
+    setNotifications(updatedNotifications);
+  };
+
+  const handleRemoveNotification = (notificationId:number) => {
+    // Handle remove logic for notifications here
+    // For example, you can filter out the notification with the given notificationId
+    const updatedNotifications = notifications.filter((notification) => notification.id !== notificationId);
+    setNotifications(updatedNotifications);
+  };
+
+  // let [storedata, SetStoreData] = useState(local_varaiable);
+
+  // //full screen
+  // const [isFullscreen, setIsFullscreen] = useState(false);
+
+  // const toggleFullscreen = () => {
+  //   const element = document.documentElement;
+  //   if (
+  //     !document.fullscreenElement &&
+  //     !document.mozFullScreenElement &&
+  //     !document.webkitFullscreenElement
+  //   ) {
+  //     // Enter fullscreen mode
+  //     if (element.requestFullscreen) {
+  //       element.requestFullscreen();
+  //     } else if (element.mozRequestFullScreen) {
+  //       element.mozRequestFullScreen();
+  //     } else if (element.webkitRequestFullscreen) {
+  //       element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+  //     }
+  //   } else {
+  //     // Exit fullscreen mode
+  //     if (document.exitFullscreen) {
+  //       document.exitFullscreen();
+  //     } else if (document.mozCancelFullScreen) {
+  //       document.mozCancelFullScreen();
+  //     } else if (document.webkitExitFullscreen) {
+  //       document.webkitExitFullscreen();
+  //     }
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   const fullscreenChangeHandler = () => {
+  //     setIsFullscreen(!!document.fullscreenElement);
+  //   };
+
+  //   document.addEventListener('fullscreenchange', fullscreenChangeHandler);
+
+  //   return () => {
+  //     document.removeEventListener('fullscreenchange', fullscreenChangeHandler);
+  //   };
+  // }, []);
+  
+
+
   return (
     <div>Header</div>
   )
