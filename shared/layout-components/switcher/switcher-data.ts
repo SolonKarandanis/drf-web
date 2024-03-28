bc50319b641249ef2c3e702e171470c11e1cde93
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-28 08:58:09 +0200

    Implementing switcher-data.ts

--- shared/layout-components/switcher/switcher-data.ts
+++ shared/layout-components/switcher/switcher-data.ts
@@ -27,3 +27,101 @@ export function Dark(actionfunction:ActionCreatorWithPayload<ThemeState, "theme/
     localStorage.removeItem("inputBorder");
     localStorage.removeItem("Light");
 }
+
+export function Light(actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "class": "light",
+        "dataHeaderStyles": "light",
+        "bodyBg": "",
+        "darkBg": "",
+        "inputBorder": "",
+        "Light": "",
+        "dataMenuStyles": theme.dataNavLayout == 'horizontal' ? '' : "dark"
+    }))
+    localStorage.setItem("ynexlighttheme", "light");
+    localStorage.removeItem("ynexdarktheme");
+    localStorage.removeItem("bodyBgRGB");
+    localStorage.removeItem("primaryRGB");
+    localStorage.removeItem("primaryRGB1");
+    localStorage.removeItem("inputBorder");
+    localStorage.removeItem("Light");
+
+}
+
+export function Ltr(actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({ ...theme, "dir": "ltr" }))
+    localStorage.removeItem("ynexrtl");
+};
+export function Rtl(actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({ ...theme, "dir": "rtl", }))
+    localStorage.setItem("ynexrtl", "rtl");
+};
+export const Vertical = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavLayout": "vertical",
+        "dataMenuStyles": "dark",
+        "dataVerticalStyle": "overlay",
+        "dataToggled": "",
+        "dataNavStyle": ''
+    }))
+    localStorage.setItem("ynexlayout", "vertical");
+    localStorage.removeItem("ynexnavstyles");
+
+};
+
+export const HorizontalClick = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    setTimeout(() => {
+        const element:HTMLElement|null = document.querySelector(".main-content");
+        if (element){
+            element.click();
+        }
+    }, 100);
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavLayout": "horizontal",
+        "dataMenuStyles": localStorage.ynexdarktheme ? 'dark' : localStorage.darkBgRGB ? 'dark' : localStorage.ynexMenu ? localStorage.ynexMenu : "light",
+        "dataVerticalStyle": "",
+        "dataNavStyle": localStorage.ynexnavstyles ? localStorage.ynexnavstyles : "menu-click"
+
+    }))
+    localStorage.setItem("ynexlayout", "horizontal");
+    localStorage.removeItem("ynexverticalstyles");
+};
+export const Menuclick = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavStyle": "menu-click",
+        "dataToggled": "menu-click-closed",
+        "dataVerticalStyle": ""
+
+    }))
+    localStorage.setItem("ynexnavstyles", "menu-click");
+
+};
+export const MenuHover = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavStyle": "menu-hover",
+        "dataVerticalStyle": "",
+        "dataToggled": "menu-hover-closed",
+        "horStyle": ""
+    }))
+    localStorage.setItem("ynexnavstyles", "menu-hover");
+    localStorage.removeItem("ynexverticalstyles");
+};
