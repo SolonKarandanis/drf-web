177e74b0065d03efdf3724c7c25e6aae9a9471c8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-28 10:08:33 +0200

    Creating switcher-data.ts

--- shared/layout-components/switcher/switcher-data.ts
+++ shared/layout-components/switcher/switcher-data.ts
@@ -125,3 +125,188 @@ export const MenuHover = (actionfunction:ActionCreatorWithPayload<ThemeState, "t
     localStorage.setItem("ynexnavstyles", "menu-hover");
     localStorage.removeItem("ynexverticalstyles");
 };
+
+export const IconClick = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataVerticalStyle": "",
+        "dataNavStyle": "icon-click",
+        "dataToggled": "icon-click-closed",
+    }))
+    localStorage.setItem("ynexnavstyles", "icon-click");
+    localStorage.removeItem("ynexverticalstyles");
+    const Sidebar:HTMLElement|null = document.querySelector(".main-menu");
+    if(Sidebar){
+        Sidebar.style.marginInline = "0px";
+    }
+    
+};
+export const IconHover = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    setTimeout(() => {
+        const ulElement:HTMLElement|null = document?.querySelector(".slide.has-sub.open ul");
+      
+        ulElement && (
+          ulElement.classList.toggle("child1"),
+          ulElement.style.display = ulElement.classList.contains("child1") ? "block" : "none"
+        );
+      }, 100);
+    dispatch(actionfunction({
+        ...theme,
+        "dataVerticalStyle": "",
+        "dataNavStyle": "icon-hover",
+        "dataToggled": "icon-hover-closed"
+    }))
+    localStorage.setItem("ynexnavstyles", "icon-hover");
+    localStorage.removeItem("ynexverticalstyles");
+    const Sidebar:HTMLElement|null = document.querySelector(".main-menu");
+    if(Sidebar){
+        Sidebar.style.marginInline = "0px";
+    }
+};
+
+export const Regular = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataPageStyle": "regular"
+    }));
+    localStorage.setItem("ynexregular", "Regular");
+    localStorage.removeItem("ynexclassic");
+    localStorage.removeItem("ynexmodern");
+};
+
+export const Classic = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataPageStyle": "classic",
+    }));
+    localStorage.setItem("ynexclassic", "Classic");
+    localStorage.removeItem("ynexregular");
+    localStorage.removeItem("ynexmodern");
+};
+export const Modern = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataPageStyle": "modern",
+    }));
+    localStorage.setItem("ynexmodern", "Modern");
+    localStorage.removeItem("ynexregular");
+    localStorage.removeItem("ynexclassic");
+};
+export function Enable(actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "loader": "enable"
+    }));
+    localStorage.setItem("ynexloaderenable", "enable");
+    localStorage.removeItem("ynexloaderdisable");
+}
+export function Disable(actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "loader": "disable"
+    }));
+    localStorage.setItem("ynexloaderdisable", "disable");
+    localStorage.removeItem("ynexloaderenable");
+}
+
+export const Fullwidth = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataWidth": "fullwidth",
+    }))
+    localStorage.setItem("ynexfullwidth", "Fullwidth");
+    localStorage.removeItem("ynexboxed");
+
+};
+export const Boxed = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataWidth": "boxed",
+    }))
+    localStorage.setItem("ynexboxed", "Boxed");
+    localStorage.removeItem("ynexfullwidth");
+};
+export const FixedMenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataMenuPosition": "fixed",
+    }))
+    localStorage.setItem("ynexmenufixed", "MenuFixed");
+    localStorage.removeItem("ynexmenuscrollable");
+};
+
+export const ScrollMenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataMenuPosition": "scrollable",
+    }))
+    localStorage.setItem("ynexmenuscrollable", "Menuscrolled");
+    localStorage.removeItem("ynexmenufixed")
+};
+
+export const Headerpostionfixed = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataHeaderPosition": "fixed",
+    }))
+    localStorage.setItem("ynexheaderfixed", 'FixedHeader');
+    localStorage.removeItem("ynexheaderscrollable");
+};
+export const Headerpostionscroll = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataHeaderPosition": "scrollable",
+    }))
+    localStorage.setItem("ynexheaderscrollable", "ScrollableHeader");
+    localStorage.removeItem("ynexheaderfixed");
+};
+
+export const Defaultmenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataVerticalStyle": "overlay",
+        "dataNavLayout": "vertical",
+        "dataToggled": "closed"
+    }))
+    localStorage.removeItem("ynexverticalstyles");
+};
+export const Closedmenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavLayout": "vertical",
+        "dataVerticalStyle": "closed",
+        "dataToggled": "close-menu-close"
+    }))
+    localStorage.setItem("ynexverticalstyles", "closed");
+
+};
