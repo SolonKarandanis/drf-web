d49188496300eea8560d34f75d714e35cf30d4fc
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-28 10:16:11 +0200

    Implementing switcher-data.ts

--- shared/layout-components/switcher/switcher-data.ts
+++ shared/layout-components/switcher/switcher-data.ts
@@ -308,5 +308,227 @@ export const Closedmenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "
         "dataToggled": "close-menu-close"
     }))
     localStorage.setItem("ynexverticalstyles", "closed");
+};
 
+export const IconText = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavLayout": "vertical",
+        "dataVerticalStyle": "icontext",
+        "dataToggled": "icon-text-close"
+    }))
+    localStorage.setItem("ynexverticalstyles", "icontext");
+};
+
+export const IconOverayFn = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavLayout": "vertical",
+        "dataVerticalStyle": "overlay",
+        "dataToggled": "icon-overlay-close",
+    }))
+    localStorage.setItem("ynexverticalstyles", "overlay");
+};
+
+export const DetachedFn = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavLayout": "vertical",
+        "dataVerticalStyle": "detached",
+        "dataToggled": "detached-close",
+    }))
+    localStorage.setItem("ynexverticalstyles", "detached");
+};
+export const DoubletFn = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataNavLayout": "vertical",
+        "dataVerticalStyle": "doublemenu",
+        "dataToggled": "double-menu-open",
+        "dataNavStyle": "",
+    }))
+    localStorage.setItem("ynexverticalstyles", "doublemenu");
+
+    setTimeout(() => {
+        if (!document.querySelector(".main-menu .has-sub.open")) {
+            dispatch(actionfunction(
+                {
+                    ...theme,
+                    "dataToggled": "double-menu-close"
+                }
+            ));
+        }
+    }, 100);
+}
+
+export const ColorMenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataMenuStyles": "color",
+
+
+    }))
+    localStorage.setItem("ynexMenu", "color");
+    localStorage.removeItem("gradient");
+};
+
+export const LightMenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataMenuStyles": "light",
+    }))
+    localStorage.setItem("ynexMenu", "light");
+    localStorage.removeItem("light");
+};
+
+export const DarkMenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataMenuStyles": "dark",
+    }))
+    localStorage.setItem("ynexMenu", "dark");
+    localStorage.removeItem("light");
+};
+
+export const GradientMenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataMenuStyles": "gradient",
+    }))
+    localStorage.setItem("ynexMenu", "gradient");
+    localStorage.removeItem("color");
+};
+export const TransparentMenu = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataMenuStyles": "transparent",
+    }))
+    localStorage.setItem("ynexMenu", "transparent");
+    localStorage.removeItem("gradient");
+};
+
+export const LightHeader = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataHeaderStyles": "light",
+    }))
+    localStorage.setItem("ynexHeader", "light");
+    localStorage.removeItem("dark");
+};
+export const DarkHeader = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataHeaderStyles": "dark",
+    }))
+    localStorage.setItem("ynexHeader", "dark");
+    localStorage.removeItem("light");
+};
+
+export const ColorHeader = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataHeaderStyles": "color",
+    }))
+    localStorage.removeItem("dark");
+    localStorage.setItem("ynexHeader", "color");
+};
+export const GradientHeader = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataHeaderStyles": "gradient",
+    }))
+    localStorage.removeItem("transparent");
+    localStorage.setItem("ynexHeader", "gradient");
+};
+export const TransparentHeader = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "dataHeaderStyles": "transparent",
+    }))
+    localStorage.removeItem("gradient");
+    localStorage.setItem("ynexHeader", "transparent");
+};
+
+export const PrimaryColor1 = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "colorPrimaryRgb": "58, 88, 146",
+        "colorPrimary": "58 88 146"
+    }));
+    localStorage.setItem("primaryRGB", "58, 88, 146");
+    localStorage.setItem("primaryRGB1", "58 88 146");
+};
+export const PrimaryColor2 = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "colorPrimaryRgb": "92, 144, 163",
+        "colorPrimary": "92 144 163"
+    }));
+    localStorage.setItem("primaryRGB", "92, 144, 163");
+    localStorage.setItem("primaryRGB1", "92 144 163");
+};
+export const PrimaryColor3 = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "colorPrimaryRgb": "161, 90, 223",
+        "colorPrimary": "161 90 223"
+    }));
+    localStorage.setItem("primaryRGB", "161, 90, 223");
+    localStorage.setItem("primaryRGB1", "161 90 223");
+};
+export const PrimaryColor4 = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "colorPrimaryRgb": "78, 172, 76",
+        "colorPrimary": "78 172 76"
+    }));
+    localStorage.setItem("primaryRGB", "78, 172, 76");
+    localStorage.setItem("primaryRGB1", "78 172 76");
+};
+export const PrimaryColor5 = (actionfunction:ActionCreatorWithPayload<ThemeState, "theme/ThemeChanger">) => {
+    const dispatch = useAppDispatch();
+    const [theme] = useThemeState();
+    dispatch(actionfunction({
+        ...theme,
+        "colorPrimaryRgb": "223, 90, 90",
+        "colorPrimary": "223 90 90"
+    }));
+    localStorage.setItem("primaryRGB", "223, 90, 90");
+    localStorage.setItem("primaryRGB1", "223 90 90");
 };
