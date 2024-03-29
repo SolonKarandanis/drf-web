137117c365840fb84f084099b3328cfe4d37adef
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-03-29 14:40:17 +0200

    added them in postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // 'postcss-import': {},
+    'postcss-custom-media': {},
+    'postcss-media-minmax': {},
+    'postcss-mixins': {},
   },
 }
