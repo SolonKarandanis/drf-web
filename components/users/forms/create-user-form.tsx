f975e777c113a5179332bd58c23d2337083c25e0
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-04-19 10:10:27 +0300

    Implementing CreateUserForm

--- components/users/forms/create-user-form.tsx
+++ components/users/forms/create-user-form.tsx
@@ -2,14 +2,24 @@
 
 import * as z from "zod";
 import { useState } from "react";
+import { motion } from 'framer-motion'
 import { CreateUserSchema } from "@/schemas/auth.schemas";
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { toast } from 'react-toastify';
+import Stepper from "@/shared/components/stepper/stepper";
+import CForm from "@/shared/components/form/cform";
 
 type Inputs = z.infer<typeof CreateUserSchema>
 
-const steps = [
+type Steps ={
+    id:string;
+    name:string;
+    fields?:string[];
+}
+
+const steps:Steps[] = [
     {
       id: 'Step 1',
       name: 'Personal Information',
@@ -26,7 +36,7 @@ const steps = [
         fields: ['username', 'password', 'confirmPassword']
     },
     { id: 'Step 4', name: 'Complete' }
-  ]
+]
 
 const CreateUserForm = () => {
     const [previousStep, setPreviousStep] = useState(0)
@@ -44,7 +54,7 @@ const CreateUserForm = () => {
         resolver: zodResolver(CreateUserSchema)
     })
 
-    const processForm: SubmitHandler<Inputs> = data => {
+    const onSubmit: SubmitHandler<Inputs> = data => {
         console.log(data)
         reset()
     }
@@ -59,7 +69,7 @@ const CreateUserForm = () => {
 
         if (currentStep < steps.length - 1) {
             if (currentStep === steps.length - 2) {
-            await handleSubmit(processForm)()
+            await handleSubmit(onSubmit)()
             }
             setPreviousStep(currentStep)
             setCurrentStep(step => step + 1)
@@ -73,41 +83,22 @@ const CreateUserForm = () => {
         }
     }
 
+    const handleError =(error:ErrorResponse)=>{
+		const {status, data:{detail}} = error;
+		toast.error(`(${status}) ${detail}`);
+	}
+
     return (
         <section className='inset-0 flex flex-col justify-between p-24 '>
-            <nav aria-label='Progress'>
-                <ol role='list' className='space-y-4 md:flex md:space-x-8 md:space-y-0'>
-                    {steps.map((step, index) => (
-                        <li key={step.name} className='md:flex-1'>
-                        {currentStep > index ? (
-                            <div className='flex flex-col w-full py-2 pl-4 transition-colors border-l-4 group border-sky-600 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4'>
-                            <span className='text-sm font-medium transition-colors text-sky-600 '>
-                                {step.id}
-                            </span>
-                            <span className='text-sm font-medium'>{step.name}</span>
-                            </div>
-                        ) : currentStep === index ? (
-                            <div
-                            className='flex flex-col w-full py-2 pl-4 border-l-4 border-sky-600 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4'
-                            aria-current='step'
-                            >
-                            <span className='text-sm font-medium text-sky-600'>
-                                {step.id}
-                            </span>
-                            <span className='text-sm font-medium'>{step.name}</span>
-                            </div>
-                        ) : (
-                            <div className='flex flex-col w-full py-2 pl-4 transition-colors border-l-4 border-gray-200 group md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4'>
-                            <span className='text-sm font-medium text-gray-500 transition-colors'>
-                                {step.id}
-                            </span>
-                            <span className='text-sm font-medium'>{step.name}</span>
-                            </div>
-                        )}
-                        </li>
-                    ))}
-                </ol>
-            </nav>
+            <Stepper steps={steps}  currentStep={currentStep}/>
+            <CForm className='py-12 mt-12' onSubmit={handleSubmit(onSubmit)}>
+                <h2 className='text-base font-semibold leading-7 text-gray-900'>
+                Personal Information
+                </h2>
+                <p className='mt-1 text-sm leading-6 text-gray-600'>
+                Provide your personal details.
+                </p>
+            </CForm>   
         </section>
     )
 }
