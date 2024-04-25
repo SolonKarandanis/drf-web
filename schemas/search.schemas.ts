e69acda05475b719c2583242c8ae05a45cbfe1e4
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-04-25 10:23:09 +0300

    Created search.schemas.ts

---
+++ schemas/search.schemas.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const UserSearchSchema = z.object({
+    username: z.string().optional(),
+    name: z.string().optional(),
+    email: z.string().optional(),
+    isActive: z.boolean().optional(),
+});
