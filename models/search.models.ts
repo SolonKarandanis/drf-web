2fe4c80af7b18dea2b6e228536398b3a926098f8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-04-25 09:03:17 +0300

    Added search models

--- models/search.models.ts
+++ models/search.models.ts
@@ -1,3 +1,26 @@
+export enum SortDirection {
+    ASC = 'ASC',
+    DESC = 'DESC',
+}
+  
+export interface Paging {
+    page: number;
+    limit: number;
+    sortField?: string;
+    sortDirection?: SortDirection;
+}
+
+export interface SearchRequest {
+    paging: Paging;
+}
+
+export interface UserSearchRequest extends SearchRequest {
+    username?: string;
+    name?: string;
+    email?: string;
+    isActive?: boolean;
+}
+
 interface SearchResponse{
     count: number;
     previous: number;
