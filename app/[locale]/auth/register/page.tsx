abfb4073b5c131e12a1d98e031f52c1d59f985ed
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-01 08:17:43 +0300

    Implementing Register form i18n

--- app/[locale]/auth/register/page.tsx
+++ app/[locale]/auth/register/page.tsx
@@ -1,11 +1,12 @@
 import RegisterForm from '@/components/auth/forms/register-form';
 import Social from '@/components/auth/social';
 import Seo from '@/shared/layout-components/seo/Seo'
-import { useLocale } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import Link from 'next/link';
 
 const page = () => {
     const locale = useLocale();
+    const t = useTranslations('REGISTER.PAGE');
     return (
         <>
         <Seo title={"Signup"}/>
@@ -16,18 +17,18 @@ const page = () => {
                         <div className="col-span-12 xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-6 sm:col-span-8">
                             <div className="box">
                                 <div className="box-body !p-[3rem]">
-                                    <p className="mb-2 font-semibold text-center h5">Sign Up</p>
+                                    <p className="mb-2 font-semibold text-center h5">{t("title")}</p>
                                     <p className="mb-4 text-[#8c9097] dark:text-white/50 opacity-[0.7] font-normal text-center">
-                                        Welcome &amp; Join us by creating a free account !
+                                        {t("description")}
                                     </p>
                                     <RegisterForm />
                                     <div className="text-center">
-                                        <p className="text-[0.75rem] text-[#8c9097] dark:text-white/50 mt-4">Already have an account? 
-                                            <Link href={`/${locale}/auth/login`} className="text-sky-900">Sign In</Link>
+                                        <p className="text-[0.75rem] text-[#8c9097] dark:text-white/50 mt-4">{t("already-have-account")}
+                                            <Link href={`/${locale}/auth/login`} className="text-sky-900">{t("sign-in")}</Link>
                                         </p>
                                     </div>
                                     <div className="my-4 text-center authentication-barrier">
-                                        <span>OR</span>
+                                        <span>{t("or")}</span>
                                     </div>
                                     <Social />
                                 </div>
