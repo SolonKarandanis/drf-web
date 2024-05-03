4d6765ea0d9c01e21ed06d94389740562d03133c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-03 11:04:32 +0300

    Created UserDetailsPage component

---
+++ app/[locale]/(protected)/users/[userId]/page.tsx
@@ -0,0 +1,22 @@
+import {FC} from 'react'
+
+interface Props{
+    params:{
+        userId:number;
+    }
+}
+
+const UserDetailsPage:FC<Props> = ({params:{userId}}) => {
+  return (
+    <div>{userId}</div>
+  )
+}
+
+export default UserDetailsPage
+
+
+// export async function generateStaticParams(){
+//     //fetch all userIds
+//     const userIds =[1,2]
+//     return userIds.map((userId)=> ({userId}));
+// }
