0002236e3e6d9f8fa268379063a666960e2826a3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-05 13:19:10 +0300

    Minor refactoring

--- app/[locale]/auth/login/page.tsx
+++ app/[locale]/auth/login/page.tsx
@@ -1,7 +1,7 @@
 import LoginForm from '@/components/auth/forms/login-form';
 import Social from '@/components/auth/social';
 import { Metadata } from 'next';
-import { useLocale, useTranslations } from 'next-intl';
+import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
 export const metadata:Metadata={
@@ -15,7 +15,7 @@ export const metadata:Metadata={
 }
 
 const page = () => {
-  const t = useTranslations('LOGIN.PAGE');
+  const t = useTranslations();
   return (
     <>
        <div className="container">
@@ -25,16 +25,16 @@ const page = () => {
                     <div className="col-span-12 xxl:col-span-4 xl:col-span-4 lg:col-span-4 md:col-span-6 sm:col-span-8">
                         <div className="box">
                             <div className="box-body !p-[3rem]">
-                              <p className="mb-2 font-semibold text-center h5">{t("title")}</p>
-                              <p className="mb-4 text-[#8c9097] dark:text-white/50 opacity-[0.7] font-normal text-center">{t("description")}</p>
+                              <p className="mb-2 font-semibold text-center h5">{t("LOGIN.PAGE.title")}</p>
+                              <p className="mb-4 text-[#8c9097] dark:text-white/50 opacity-[0.7] font-normal text-center">{t("LOGIN.PAGE.description")}</p>
                               <LoginForm />
                               <div className="text-center">
-                                <p className="text-[0.75rem] text-[#8c9097] dark:text-white/50 mt-4">{t("dont-have-account")}
-                                  <Link href='/auth/register' className="text-sky-900">{t("sign-up")}</Link>
+                                <p className="text-[0.75rem] text-[#8c9097] dark:text-white/50 mt-4">{t("LOGIN.PAGE.dont-have-account")}
+                                  <Link href='/auth/register' className="text-sky-900">{t("LOGIN.PAGE.sign-up")}</Link>
                                 </p>
                               </div>
                               <div className="my-4 text-center authentication-barrier">
-                                  <span>{t("or")}</span>
+                                  <span>{t("GLOBAL.OR")}</span>
                               </div>
                               <Social />
                             </div>
