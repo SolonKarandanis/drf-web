731576090098234d0e81f5f7d894215d82078d20
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-09 09:23:58 +0300

    Implemented table

--- app/[locale]/(protected)/users/search/columns.tsx
+++ app/[locale]/(protected)/users/search/columns.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ColumnDef } from '@tanstack/react-table'
-
+import { MoreHorizontal, ArrowUpDown } from 'lucide-react'
 import { Button } from '@/shared/shadcn/components/ui/button' 
 import {
   DropdownMenu,
@@ -44,6 +44,39 @@ export const columns: ColumnDef<UserTableRow>[] =[
     },
     {
         header:"Created",
-        accessorKey:"created_date"
+        accessorKey:"created_date",
+        cell: ({ row }) => {
+            const date = new Date(row.getValue('created_date'))
+            const formatted = date.toLocaleDateString()
+            return <div className='font-medium'>{formatted}</div>
+        }
     },
+    {
+        id: 'actions',
+        cell: ({ row }) => {
+            const user = row.original
+
+            return (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant='ghost' className='h-8 w-8 p-0'>
+                      <span className='sr-only'>Open menu</span>
+                      <MoreHorizontal className='h-4 w-4' />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align='end'>
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem
+                      onClick={() => navigator.clipboard.writeText(String(user.id))}
+                    >
+                      Copy user ID
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>View customer</DropdownMenuItem>
+                    <DropdownMenuItem>View payment details</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )
+        }
+    }
 ]
