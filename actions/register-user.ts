dd192d2d52a053c45311fbb475ab8cfe2183187b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-09 10:53:58 +0300

    Refactoring Register user form

--- actions/register-user.ts
+++ actions/register-user.ts
@@ -1,7 +1,31 @@
 "use server";
+import * as z from "zod";
+import { RegisterSchema } from '@/schemas/auth.schemas';
 
-export async function registerUser(request:CreateUserRequest){
-    console.log(request);
+
+type RegisterSchema = z.infer<typeof RegisterSchema>;
+
+export async function registerUser(data:RegisterSchema){
+    console.log(data);
+    const result = RegisterSchema.safeParse(data);
+    if (!result.success){
+        const errorMessages = result.error.issues.reduce((prev, issue) => {
+            return (prev += issue.message);
+        }, '');
+        return {
+            error: errorMessages,
+        };
+    }
+
+    const {username,password,email,role,firstName,lastName,confirmPassword} = data;
+    const request:CreateUserRequest={
+        email,
+        first_name:firstName,
+        last_name:lastName,
+        password,
+        username,
+        password2:confirmPassword
+    }
     // fetch("/api/register", {
     //   method: "POST",
     //   headers: {
--- components/auth/forms/register-form.tsx
+++ components/auth/forms/register-form.tsx
@@ -9,16 +9,15 @@ import Link from 'next/link';
 import CFormInput from '@/shared/components/form-input/cform-input';
 import CButton from '@/shared/components/button/cbutton';
 import { useRouter } from 'next/navigation';
-import { useRegisterUserMutation } from '@/shared/redux/features/authApiSlice';
 import { toast } from 'react-toastify';
 import { useAppDispatch } from '@/shared/redux/hooks';
-import { setLoading } from '@/shared/redux/features/authSlice';
 import CFormSelect from '@/shared/components/form-select/cform-select';
 import { useTranslations } from 'next-intl';
 import { useEffect, useState } from 'react';
 import { passwordStrength } from "check-password-strength";
 import PasswordStrength from '@/shared/components/password-strength/password-strength';
 import { ErrorResponse } from '@/models/error.models';
+import { registerUser } from '@/actions/register-user';
 
 type RegisterSchema = z.infer<typeof RegisterSchema>;
 
@@ -37,11 +36,8 @@ const RegisterForm = () => {
     const t = useTranslations();
     const rform='REGISTER.FORM';
     const router = useRouter();
-    const [registerUser, { isLoading }] = useRegisterUserMutation();
+    // const [registerUser, { isLoading }] = useRegisterUserMutation();
     const dispatch = useAppDispatch();
-    
-
-    
 
     const {
         register,
@@ -75,18 +71,7 @@ const RegisterForm = () => {
 	}
 
     const onSubmit:SubmitHandler<RegisterSchema> = async (values: RegisterSchema) =>{
-        const {username,password,email,role,firstName,lastName,confirmPassword} = values;
-
-        const request:CreateUserRequest={
-            email,
-            first_name:firstName,
-            last_name:lastName,
-            password,
-            username,
-            password2:confirmPassword
-        }
-
-        const response = await registerUser(request);
+        const response = await registerUser(values);
         // if (response?.error) {
         //     toast.error(response.error);
         // } else {
