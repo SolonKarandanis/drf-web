456c1f245d54999014a5882b8ad2246638460cb2
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-12 14:33:44 +0300

    minor changes

--- shared/components/button/cbutton.test.tsx
+++ shared/components/button/cbutton.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen,fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom'
 import CButton from './cbutton';
 import { MdClear } from "react-icons/md";
 
