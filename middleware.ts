dfc33a205638714c63f95e1936d08d66509856d6
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-05-13 08:42:39 +0300

    implementing withAuthorisationMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,13 @@
 import { chain } from '@/middlewares/chain'
 import { withAuthenticationMiddleware } from '@/middlewares/authentication-middleware'
 import { withI18nMiddleware } from '@/middlewares/i18n-middleware'
+import { withAuthorisationMiddleware } from './middlewares/authorisation-middleware'
 
-export default chain([ withAuthenticationMiddleware,withI18nMiddleware])
+export default chain([ 
+  withAuthenticationMiddleware,
+  // withAuthorisationMiddleware,
+  withI18nMiddleware
+])
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico).*)']
--- middlewares/authorisation-middleware.ts
+++ middlewares/authorisation-middleware.ts
@@ -1,7 +1,9 @@
 // Ref: https://next-auth.js.org/configuration/nextjs#advanced-usage
 import { withAuth, NextRequestWithAuth } from "next-auth/middleware"
-import { NextResponse } from "next/server"
-import {getAllowedRoutes,allowedPaths} from './get-allowed-routes'
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server"
+import {getAllowedRoutes,allowedPaths} from './get-allowed-routes';
+import { CustomMiddleware } from './chain'
+import { getToken } from "next-auth/jwt";
 
 export default withAuth(
     // `withAuth` augments your `Request` with the user's token.
@@ -22,3 +24,25 @@ export default withAuth(
         },
     }
 )
+
+export function withAuthorisationMiddleware(middleware: CustomMiddleware){
+    return async (request: NextRequest, event: NextFetchEvent) =>{
+        // Create a response object to pass down the chain
+        const response = NextResponse.next()
+
+        const token = await getToken({ req: request })
+        // @ts-ignore
+        request.nextauth = request.nextauth || {}
+        // @ts-ignore
+        request.nextauth.token = token
+        const pathname = request.nextUrl.pathname
+
+        if (pathname.includes("/dashboard") && token?.role !== "admin") {
+            return NextResponse.rewrite(
+                new URL("/denied", request.url)
+            )
+        }
+
+        return middleware(request, event, response);
+    }
+}
