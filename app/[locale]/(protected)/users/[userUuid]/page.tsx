f1fdb591091912f24b69a5317878898bbfdc8259
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-01 14:42:40 +0300

    Refactoring

--- app/[locale]/(protected)/users/[userUuid]/page.tsx
+++ app/[locale]/(protected)/users/[userUuid]/page.tsx
@@ -2,12 +2,6 @@ import PageHeader from '@/shared/layout-components/page-header/PageHeader';
 import { Metadata } from 'next';
 import {FC} from 'react'
 import { basePath } from '@/next.config';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/options';
-import { getUserGroups } from '@/utils/user-utils';
-import Profile from '@/components/users/user-details/profile';
-import ContactInformation from '@/components/users/user-details/contanct-information';
-import SocialNetworks from '@/components/users/user-details/social-networks';
 import RecentActivity from '@/components/users/user-details/recent-activity';
 import PreviousOrders from '@/components/users/user-details/previous-orders';
 import UserDetails from '@/components/users/user-details/user-details';
@@ -40,10 +34,10 @@ interface Props{
 
 const UserDetailsPage:FC<Props> = async ({params:{userUuid}}) => {
   const path = process.env.NODE_ENV === "production" ? basePath : "";
-  const session = await getServerSession(authOptions);
-  const loggedInUser= session!.user!;
-  const groupNames =getUserGroups(loggedInUser);
-  const roles = groupNames.join(', ');
+  // const session = await getServerSession(authOptions);
+  // const loggedInUser= session!.user!;
+  // const groupNames =getUserGroups(loggedInUser);
+  // const roles = groupNames.join(', ');
   
   return (
     <>
