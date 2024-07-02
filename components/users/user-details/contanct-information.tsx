c82923cb394b6b15c7e5ba5db521b8cac736f33d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-02 07:34:49 +0300

    Refactored ContactInformation

--- components/users/user-details/contanct-information.tsx
+++ components/users/user-details/contanct-information.tsx
@@ -19,31 +19,46 @@ const ContactInformation:FC<Props> = ({
     zipCode,
     address
 }) => {
-    const location = `${address}, ${city}, ${state}, ${country}, ${zipCode}`
+    let location=''
+    if(address){
+        location += `${address}`
+    }
+    if(city){
+        location += `, ${city}`
+    }
+    if(state){
+        location += `, ${state}`
+    }
+    if(country){
+        location += `, ${country}`
+    }
+    if(zipCode){
+        location += `, ${zipCode}`
+    }
     return (
         <div className="p-6 border-b border-dashed dark:border-defaultborder/10">
             <p className="text-[.9375rem] mb-2 me-6 font-semibold">
             Contact Information :
             </p>
             <div className="text-[#8c9097] dark:text-white/50">
-            <p className="mb-2">
-                <span className="avatar avatar-sm avatar-rounded me-2 bg-light text-[#8c9097] dark:text-white/50">
-                    <i className="ri-mail-line align-middle text-[.875rem] text-[#8c9097] dark:text-white/50"></i>
-                </span>
-                {email}
-            </p>
-            <p className="mb-2">
-                <span className="avatar avatar-sm avatar-rounded me-2 bg-light text-[#8c9097] dark:text-white/50">
-                    <i className="ri-phone-line align-middle text-[.875rem] text-[#8c9097] dark:text-white/50"></i>
-                </span>
-                {phone}
-            </p>
-            <p className="mb-0">
-                <span className="avatar avatar-sm avatar-rounded me-2 bg-light text-[#8c9097] dark:text-white/50">
-                    <i className="ri-map-pin-line align-middle text-[.875rem] text-[#8c9097] dark:text-white/50"></i>
-                </span>
-                {location}
-            </p>
+                <p className="mb-2">
+                    <span className="avatar avatar-sm avatar-rounded me-2 bg-light text-[#8c9097] dark:text-white/50">
+                        <i className="ri-mail-line align-middle text-[.875rem] text-[#8c9097] dark:text-white/50"></i>
+                    </span>
+                    {email}
+                </p>
+                <p className="mb-2">
+                    <span className="avatar avatar-sm avatar-rounded me-2 bg-light text-[#8c9097] dark:text-white/50">
+                        <i className="ri-phone-line align-middle text-[.875rem] text-[#8c9097] dark:text-white/50"></i>
+                    </span>
+                    {phone}
+                </p>
+                <p className="mb-0">
+                    <span className="avatar avatar-sm avatar-rounded me-2 bg-light text-[#8c9097] dark:text-white/50">
+                        <i className="ri-map-pin-line align-middle text-[.875rem] text-[#8c9097] dark:text-white/50"></i>
+                    </span>
+                    {location}
+                </p>
             </div>
         </div>
     )
