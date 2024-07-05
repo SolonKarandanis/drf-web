b196cc41290d00ae3225035077bc59a271226bd6
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-05 13:53:16 +0300

    Small change

--- components/users/user-details/user-details.tsx
+++ components/users/user-details/user-details.tsx
@@ -86,7 +86,7 @@ const UserDetails:FC<Props> =  ({userUuid}) => {
         <div className="items-center justify-between p-6 border-b border-dashed dark:border-defaultborder/10 md:flex">
           <div className="w-full mb-6 ">
             <p className="text-[.9375rem] mb-2 font-semibold">Professional Bio :</p>
-            {isLoading ? (
+            {userData.isLoading ? (
                 <div role="status" className="w-full rounded animate-pulse dark:border-gray-700">
                   <div className="h-2 bg-gray-400 rounded-full dark:bg-gray-700 mb-2.5"></div>
                   <div className="h-2 bg-gray-400 rounded-full dark:bg-gray-700 mb-2.5"></div>
