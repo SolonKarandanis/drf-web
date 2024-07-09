3dd58b30c025b07cf376b279005fd03be6900d5b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-09 11:39:28 +0300

    Minor changes

--- app/[locale]/(protected)/users/[userUuid]/page.tsx
+++ app/[locale]/(protected)/users/[userUuid]/page.tsx
@@ -6,6 +6,7 @@ import PreviousOrders from '@/components/users/user-details/previous-orders';
 import UserDetails from '@/components/users/user-details/user-details';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/options';
+import Link from 'next/link';
 
 
 export const metadata:Metadata={
@@ -50,6 +51,34 @@ const UserDetailsPage:FC<Props> = async ({params:{userUuid}}) => {
           </div>
           <div className="col-span-12 xxl:col-span-8 xl:col-span-12">
             <div className="grid grid-cols-12 gap-x-6">
+              <div className="col-span-12 xl:col-span-12">
+                <div className="box">
+                  <div className="box-body !p-0">
+                    <div className="!p-4 border-b dark:border-defaultborder/10 border-dashed md:flex items-center justify-between">
+                      <nav className="-mb-0.5 sm:flex md:space-x-6 rtl:space-x-reverse pb-2">
+                          <Link className="flex w-full px-4 py-2 text-sm rounded-md sm:w-auto active hs-tab-active:font-semibold hs-tab-active:text-white hs-tab-active:bg-primary text-primary" href="#!" id="activity-tab" data-hs-tab="#activity-tab-pane" aria-controls="activity-tab-pane">
+                              <i className="inline-block align-middle ri-gift-line me-1"></i>Activity
+                          </Link>
+                          <Link className="flex w-full px-4 py-2 text-sm rounded-md sm:w-auto hs-tab-active:font-semibold hs-tab-active:text-white hs-tab-active:bg-primary text-primary" href="#!" id="posts-tab" data-hs-tab="#posts-tab-pane" aria-controls="posts-tab-pane">
+                              <i className="inline-block align-middle ri-bill-line me-1"></i>Posts
+                          </Link>
+                          <Link className="flex w-full px-4 py-2 text-sm rounded-md sm:w-auto hs-tab-active:font-semibold hs-tab-active:text-white hs-tab-active:bg-primary text-primary" href="#!" id="followers-tab" data-hs-tab="#followers-tab-pane" aria-controls="followers-tab-pane">
+                              <i className="inline-block align-middle ri-money-dollar-box-line me-1"></i>Friends
+                          </Link>
+                          <Link className="flex w-full px-4 py-2 text-sm rounded-md sm:w-auto hs-tab-active:font-semibold hs-tab-active:text-white hs-tab-active:bg-primary text-primary" href="#!" id="gallery-tab" data-hs-tab="#gallery-tab-pane" aria-controls="gallery-tab-pane">
+                              <i className="inline-block align-middle ri-exchange-box-line me-1"></i>Gallery
+                          </Link>
+                      </nav>
+                    </div>
+                    <div className="!p-4">
+                      <div className="tab-content" id="myTabContent">
+                        <div className="tab-pane show active fade !p-0 !border-0" id="activity-tab-pane"
+                                                role="tabpanel" aria-labelledby="activity-tab" ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="col-span-12 xl:col-span-4">
                 <div className="box">
                     <div className="box-header">
