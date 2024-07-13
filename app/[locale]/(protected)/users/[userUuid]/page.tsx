9c2f2a07784af62da853b12ecba009706d80c870
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-13 15:27:59 +0300

    changes

--- app/[locale]/(protected)/users/[userUuid]/page.tsx
+++ app/[locale]/(protected)/users/[userUuid]/page.tsx
@@ -92,19 +92,31 @@ const UserDetailsPage:FC<Props> = async ({params:{userUuid}}) => {
                                 <ul className="flex flex-col pl-0 mb-0 rounded-lg">
                                   <li className="relative block px-0 py-2 bg-white border-0 rounded-t-lg text-inherit">
                                     <div className="min-h-6 mb-0.5 flex pl-0">
-                                      <input id="follow" className="rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox"   />
+                                      <input id="follow" className="rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl 
+                                        after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left w-10 cursor-pointer appearance-none border 
+                                        border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all 
+                                        after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] 
+                                        checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox"   />
                                       <label htmlFor="follow" className="w-4/5 mb-0 ml-4 overflow-hidden text-sm font-normal cursor-pointer select-none text-ellipsis whitespace-nowrap text-slate-500" >Email me when someone follows me</label>
                                     </div>
                                   </li>
                                   <li className="relative block px-0 py-2 bg-white border-0 text-inherit">
                                     <div className="min-h-6 mb-0.5 flex pl-0">
-                                      <input id="answer" className="rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox" />
+                                      <input id="answer" className="rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl 
+                                        after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left w-10 cursor-pointer appearance-none border 
+                                        border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all 
+                                        after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-['']
+                                      checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox" />
                                       <label htmlFor='answer' className="w-4/5 mb-0 ml-4 overflow-hidden text-sm font-normal cursor-pointer select-none text-ellipsis whitespace-nowrap text-slate-500" >Email me when someone answers on my post</label>
                                     </div>
                                   </li>
                                   <li className="relative block px-0 py-2 bg-white border-0 rounded-b-lg text-inherit">
                                     <div className="min-h-6 mb-0.5 flex pl-0">
-                                      <input id="mention" className="rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox"  />
+                                      <input id="mention" className="rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl 
+                                        after:duration-250 checked:after:translate-x-5.25 h-5 relative float-left w-10 cursor-pointer appearance-none border 
+                                        border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all 
+                                        after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] 
+                                        checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox"  />
                                       <label htmlFor="mention" className="w-4/5 mb-0 ml-4 overflow-hidden text-sm font-normal cursor-pointer select-none text-ellipsis whitespace-nowrap text-slate-500" >Email me when someone mentions me</label>
                                     </div>
                                   </li>
