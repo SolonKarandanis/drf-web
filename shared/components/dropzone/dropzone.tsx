876685cd0ce941c4bcf0072f5f45dd14f0e45e65
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-08-29 08:45:46 +0300

    Implementing Dropzone component

--- shared/components/dropzone/dropzone.tsx
+++ shared/components/dropzone/dropzone.tsx
@@ -1,14 +1,21 @@
 "use client"
 
 import  { ChangeEvent, useState } from 'react'
+import Image from "next/image";
+import { Button } from '@/shared/shadcn/components/ui/button';
+import ButtonLoading from '../button-loading/button-loading';
 
 const Dropzone = () => {
-    const [file, setFile] = useState<File>();
+    const [file, setFile] = useState<File| null>();
 
     const handleUploadFile=(event:ChangeEvent<HTMLInputElement>)=>{
         if (!event.target.files) return;
         const fileUpload = event.target.files[0];
         setFile(fileUpload);
+
+        if(file){
+
+        }
     }
 
     return (
@@ -17,35 +24,66 @@ const Dropzone = () => {
                 <div className="px-4 py-6">
                     <div id="image-preview" 
                         className="items-center max-w-sm p-6 mx-auto mb-4 text-center bg-gray-100 border-2 border-gray-400 border-dashed rounded-lg cursor-pointer">
-                        <input id="upload" type="file" className="hidden" accept="image/*" />
-                        <label htmlFor="upload" className="flex flex-col gap-y-0.5 cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" 
-                                fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" 
-                                className="w-8 h-8 mx-auto mb-4 text-gray-700">
-                                <path strokeLinecap="round" strokeLinejoin="round" 
-                                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-                            </svg>
-                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-700">
-                                Upload picture
-                            </h5>
-                            <p className="text-sm font-normal text-gray-400 md:px-6">
-                                Choose photo size should be less than 
-                                <b className="text-gray-600">2mb</b>
-                            </p>
-                            <p className="text-sm font-normal text-gray-400 md:px-6">
-                                and should be in 
-                                <b className="text-gray-600">JPG, PNG, or GIF</b> format.
-                            </p>
-                            <span id="filename" className="z-50 text-gray-500 bg-gray-200"></span>
-                        </label>
+                        {file ? 
+                            (
+                                <Image
+                                    src={URL.createObjectURL(file)}
+                                    alt={file.name}
+                                    sizes="100vw"
+                                    width={700}
+                                    height={475}
+                                />
+                            ):
+                            
+                            (
+                                <>
+                                    <input id="upload" type="file" className="hidden" accept="image/*" onChange={handleUploadFile}/>
+                                    <label htmlFor="upload" className="flex flex-col gap-y-0.5 cursor-pointer">
+                                    <svg xmlns="http://www.w3.org/2000/svg" 
+                                        fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" 
+                                        className="w-8 h-8 mx-auto mb-4 text-gray-700">
+                                        <path strokeLinecap="round" strokeLinejoin="round" 
+                                            d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                                    </svg>
+                                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-700">
+                                        Upload picture
+                                    </h5>
+                                    <p className="text-sm font-normal text-gray-400 md:px-6">
+                                        Choose photo size should be less than 
+                                        <b className="text-gray-600">2mb</b>
+                                    </p>
+                                    <p className="text-sm font-normal text-gray-400 md:px-6">
+                                        and should be in 
+                                        <b className="text-gray-600">JPG, PNG, or GIF</b> format.
+                                    </p>
+                                    <span id="filename" className="z-50 text-gray-500 bg-gray-200"></span>
+                                    </label>
+                                </>
+                            )
+                        
+                        }
+                        
                     </div>
-                    <div className="flex items-center justify-center">
-                        <div className="w-full">
-                            <label className="w-full text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 
-                                focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center mr-2 mb-2 cursor-pointer">
-                                <span className="ml-2 text-center">Upload</span>
-                            </label>
-                        </div>
+                    <div className="flex flex-col items-center justify-center w-full gap-y-2">
+                            <Button 
+                                type="submit"
+                                className="w-full text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 
+                                    focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center  mb-2 cursor-pointer"
+                                >
+                                    <span className="ml-2 text-center">Upload</span>
+                                {/* {isLoading ? 
+                                    <ButtonLoading /> : 
+                                    Upload
+                                } */}
+                                
+                            </Button>
+                            <Button 
+                                type="reset" 
+                                variant="destructive"
+                                className="w-full"
+                                onClick={()=>setFile(null)}>
+                                Clear
+                            </Button>
                     </div>
                 </div>
             </div>
