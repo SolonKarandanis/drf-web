1ddc78efcafbdc7863566b8e1f26e06d12c9b00a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-02 15:45:52 +0300

    Created UserEditGroupButtons

---
+++ components/users/user-details/user-edit-group-buttons.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@/shared/shadcn/components/ui/button';
+import {FC, MouseEventHandler} from 'react'
+
+interface Props{
+    onCancelClick:MouseEventHandler<HTMLButtonElement>;
+    onSaveClick:MouseEventHandler<HTMLButtonElement>;
+}
+
+const UserEditGroupButtons:FC<Props> = ({
+    onCancelClick,
+    onSaveClick
+}) => {
+  return (
+    <div className="w-44"  data-testid="buttons">
+        <div className='flex justify-between'>
+            <Button 
+                type="reset" 
+                variant="danger"
+                className="w-20"
+                onClick={onCancelClick}>
+                Cancel
+            </Button>
+            <Button 
+                type="button" 
+                variant="success"
+                className="w-20"
+                onClick={onSaveClick}>
+                Save 
+            </Button>
+        </div>
+    </div>
+  )
+}
+
+export default UserEditGroupButtons
