7ec4e62ff5ba4e18cfed977cb8a169e3b29b6fea
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-03 07:46:56 +0300

    Small change in schema

--- schemas/social.schemas.ts
+++ schemas/social.schemas.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const CreateUserSocialsSchema = z.object({
     userId: z.number(),
     socialId: z.number(),
-    url: z.string()
+    url: z.string().url()
 });
 
 export const DeleteSocialUserSchema = z.object({
