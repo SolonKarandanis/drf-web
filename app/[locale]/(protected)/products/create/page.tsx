934752a951dd63e985c72eae1093fd238a5531b5
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-09 12:59:15 +0300

    Implementing create product page

--- app/[locale]/(protected)/products/create/page.tsx
+++ app/[locale]/(protected)/products/create/page.tsx
@@ -1,9 +1,13 @@
 import PageHeader from "@/shared/layout-components/page-header/PageHeader";
 const Select = dynamic(() => import("react-select"), { ssr: false });
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import { Metadata } from "next";
 import dynamic from 'next/dynamic';
 import { useTranslations } from "next-intl";
+import { FilePond, registerPlugin } from 'react-filepond';
+import DatePicker from 'react-datepicker';
+// import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+// import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+// registerPlugin(FilePondPluginImagePreview, FilePondPluginImageExifOrientation);
 
 export const metadata:Metadata={
     title:"Drf Create Product",
@@ -170,15 +174,140 @@ const CreateProductPage =() =>{
                                                         *Description should not exceed 500 letters
                                                     </label>
                                                 </div>
-                                                <div className="col-span-12 mb-4 xl:col-span-12">
-                                                    <label className="form-label">Product Features</label>
-                                                    <div id="product-features">
-                                                        <ReactQuill />
-                                                    </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-span-12 xxl:col-span-6 xl:col-span-12 lg:col-span-12 md:col-span-6">
+                                            <div className="grid grid-cols-12 gap-4">
+                                                <div className="col-span-12 xl:col-span-4">
+                                                    <label 
+                                                        htmlFor="product-actual-price" 
+                                                        className="form-label">
+                                                        Actual Price
+                                                    </label>
+                                                    <input 
+                                                        type="text" 
+                                                        className="form-control w-full !rounded-md" 
+                                                        id="product-actual-price" 
+                                                        placeholder="Actual Price" />
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-4">
+                                                    <label 
+                                                        htmlFor="product-dealer-price" 
+                                                        className="form-label">
+                                                        Dealer Price
+                                                    </label>
+                                                    <input 
+                                                        type="text" 
+                                                        className="form-control w-full !rounded-md" 
+                                                        id="product-dealer-price" 
+                                                        placeholder="Dealer Price" />
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-4">
+                                                    <label 
+                                                        htmlFor="product-discount" 
+                                                        className="form-label">
+                                                        Discount
+                                                    </label>
+                                                    <input 
+                                                        type="text" 
+                                                        className="form-control w-full !rounded-md" 
+                                                        id="product-discount" 
+                                                        placeholder="Discount in %" />
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-6">
+                                                    <label 
+                                                        htmlFor="product-type" 
+                                                        className="form-label">
+                                                        Product Type
+                                                    </label>
+                                                    <input 
+                                                        type="text" 
+                                                        className="form-control w-full !rounded-md" 
+                                                        id="product-type" 
+                                                        placeholder="Type" />
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-6">
+                                                    <label 
+                                                        htmlFor="product-discount" 
+                                                        className="form-label">
+                                                        Item Weight
+                                                    </label>
+                                                    <input 
+                                                        type="text" 
+                                                        className="form-control w-full !rounded-md" 
+                                                        id="product-discount1" 
+                                                        placeholder="Weight in gms" />
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-12 product-documents-container">
+                                                    <p className="font-semibold mb-2 text-[0.875rem]">Product Images :</p>
+                                                {/* <FilePond className="basic-filepond" accepted-file-types={["application/pdf", "image/png", "image/jpeg", "image/gif"]}
+                                                    server="/api" allowReorder={true} files={files} onupdatefiles={setFiles} allowMultiple={false} allowImagePreview={true} maxFiles={10} name="filepond"
+                                                    labelIdle='Drag & Drop your files or <span className="filepond--label-action">Browse</span>' /> */}
+                                                </div>
+                                                <label className="form-label opacity-[0.5] mt-4 xl:col-span-12 col-span-12">Minimum 0f 6 images are need to be uploaded,make sure the image size match the proper background size and all images should be uniformly maintained with width and height to the image container,image size should not exceed 2MB,once uploaded to change the image you need to wait minimum of 24hrs. </label>
+                                                <div className="col-span-12 xl:col-span-12 product-documents-container">
+                                                    <p className="font-semibold mb-2 text-[0.875rem]">Warrenty Documents :</p>
+                                                    {/* <FilePond className="w-full product-documents"
+                                                        files={files1}
+                                                        onupdatefiles={setFiles1}
+                                                        allowMultiple={true}
+                                                        // maxFiles={6}
+                                                        server="/api"
+                                                        name="files"
+                                                        labelIdle='Drag & Drop your file here or click '
+                                                    /> */}
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-6">
+                                                    <label htmlFor="publish-date" className="form-label">Publish Date</label>
+                                                    {/* <DatePicker selected={startDate} onChange={handleDateChange} /> */}
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-6">
+                                                    <label htmlFor="publish-time" className="form-label">Publish Time</label>
+                                                    {/* <DatePicker
+                                                        selected={startDate}
+                                                        onChange={handleDateChange}
+                                                        showTimeSelect
+                                                        showTimeSelectOnly
+                                                        timeIntervals={15}
+                                                        timeCaption="Time"
+                                                        dateFormat="h:mm aa"
+                                                    /> */}
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-6">
+                                                    <label htmlFor="product-status-add" className="form-label">Published Status</label>
+                                                    {/* <Select name="product-status-add" options={Addproduct5} className="w-full !rounded-md" isSearchable
+                                                        menuPlacement='auto' classNamePrefix="Select2" placeholder="Select" id="product-status-add"
+                                                    /> */}
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-6">
+                                                    <label htmlFor="product-tags" className="form-label">Product Tags</label>
+                                                    {/* <Select isMulti name="product-tags" options={Addproduct6} defaultValue={[Addproduct6[0], Addproduct6[3]]}
+                                                        className="w-full !rounded-md" isSearchable id="product-tags"
+                                                        menuPlacement='auto' classNamePrefix="Select2" placeholder="Select"
+                                                    /> */}
+                                                </div>
+                                                <div className="col-span-12 xl:col-span-12">
+                                                    <label htmlFor="product-status-add1" className="form-label">Availability</label>
+                                                    {/* <Select name="product-status-add1" options={Addproduct7} id="product-status-add1"
+                                                        className="w-full !rounded-md" isSearchable
+                                                        menuPlacement='auto' classNamePrefix="Select2" placeholder="Select"
+                                                    /> */}
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
+                                <div className="justify-end px-6 py-4 border-t border-dashed dark:border-defaultborder/10 sm:flex">
+                                    <button 
+                                        type="button" 
+                                        className="ti-btn ti-btn-primary !font-medium m-1">
+                                        Add Product<i className="bi bi-plus-lg ms-2"></i>
+                                    </button>
+                                    <button 
+                                        type="button" 
+                                        className="ti-btn ti-btn-success !font-medium m-1">
+                                        Save Product<i className="bi bi-download ms-2"></i>
+                                    </button>
                                 </div>
                             </div>
                         </div>
