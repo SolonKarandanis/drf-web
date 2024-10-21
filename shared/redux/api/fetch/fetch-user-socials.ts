6c801c682fc9d20606b71303eaa2710beb2f33ce
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-21 16:37:26 +0300

    Minor change

--- shared/redux/api/fetch/fetch-user-socials.ts
+++ shared/redux/api/fetch/fetch-user-socials.ts
@@ -1,7 +1,8 @@
-import { UserSocials } from "@/models/user.models";
+
 import { baseUrl } from "@/utils/constants";
 import { ApiControllers } from "../ApiControllers";
 import { getAccessTokenValue } from "@/utils/functions";
+import { UserSocials } from "@/models/social.models";
 
 export const fetchUserSocials = async (uuid:string): Promise<UserSocials[]> => {
     const data = await fetch(`${baseUrl}/${ApiControllers.USERS}/${uuid}`,{
