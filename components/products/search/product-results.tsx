d9c9523ffbf97ed377314d6e669eef2c2e341fdd
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-10-30 15:00:28 +0200

    Removed comments

--- components/products/search/product-results.tsx
+++ components/products/search/product-results.tsx
@@ -57,33 +57,3 @@ const ProductResults = () => {
 }
 
 export default ProductResults
-
-{/* <div className="grid grid-cols-12 gap-x-6">
-                                {Itemsdata1.map((item) =>(
-                                    <Card>
-                                        <Card.Image src={item.preview} href={`/products/${productUuid}`}/>
-                                        <Card.IconSection>
-                                            <Card.IconSection.Icon 
-                                                className="wishlist"
-                                                href={`/components/pages/ecommerce/wishlist/`}>
-                                                <i className="ri-heart-line"></i>
-                                            </Card.IconSection.Icon>
-                                            <Card.IconSection.Icon 
-                                                className="cart"
-                                                href={`/components/pages/ecommerce/cart/`}>
-                                                <i className="ri-shopping-cart-line"></i>
-                                            </Card.IconSection.Icon>
-                                            <Card.IconSection.Icon 
-                                                className="view" 
-                                                href={`/products/${productUuid}`}>
-                                                <i className="ri-eye-line"></i>
-                                            </Card.IconSection.Icon>
-                                        </Card.IconSection>
-                                        <Card.Title title={item.title}>
-                                            <Card.Title.Rating>{item.rating}</Card.Title.Rating>
-                                        </Card.Title>
-                                        <Card.Description>{item.description}</Card.Description>
-                                        <Card.Price>{item.newpr}</Card.Price>
-                                    </Card>
-                                ))}
-                            </div> */}
