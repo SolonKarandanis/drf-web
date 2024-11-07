7540d3ef567ed318e304cba57d0ea3efbd8df6ec
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-07 08:30:53 +0200

    small changes

--- components/products/hooks/useProductFilters.ts
+++ components/products/hooks/useProductFilters.ts
@@ -17,22 +17,29 @@ export function useProductFilters() {
     const router = useRouter();
     const pathname = usePathname();
     const query = searchParams.get('query') ?? '';
+    const category=searchParams.getAll('category');
     const page = Number(searchParams.get('page') ?? '1');
     const size = Number(searchParams.get('size') ?? '8');
 
     const setQuery = useCallback((query:string)=>{
-        if (query) {
-            router.push(`${pathname}?query=${query}`);
-        } else { 
-          router.push(pathname); 
+        if(query){
+            const newSearchParams = new URLSearchParams(searchParams.toString());
+            newSearchParams.set('query', query);
+            router.push(`?${newSearchParams.toString()}`);
+        }
+        else{
+            router.push(pathname); 
         }
     },[]);
 
     const setPage = useCallback((page:number)=>{
-        if (page) {
-            router.push(`${pathname}?page=${page}`);
-        } else { 
-          router.push(pathname); 
+        if(page){
+            const newSearchParams = new URLSearchParams(searchParams.toString());
+            newSearchParams.set('page', String(page));
+            router.push(`?${newSearchParams.toString()}`);
+        }
+        else{
+            router.push(pathname); 
         }
     },[]);
 
@@ -41,15 +48,12 @@ export function useProductFilters() {
             if(!Array.isArray(category)){
                 category = [category]
             }
-            const url = `${pathname}?page`;
-            for(const [i, cat] of category.entries()){
-                if (i !== category.length - 1) {
-                    router.push(`${url}=${cat}&`);
-                }
-                else{
-                    router.push(`${url}=${cat}`);
-                }
-            }
+            const params = new URLSearchParams(searchParams);
+            params.delete('category');
+            category.forEach(cat=> {
+                return params.append('category', String(cat));
+            });
+            router.push(`?${params.toString()}`);
            
         } else { 
           router.push(pathname); 
