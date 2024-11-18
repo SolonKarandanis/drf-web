e962f7fb675e980288fab7b3cd1ab7947a4bbcb9
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-18 07:45:47 +0200

    Created SimilarProducts component

---
+++ components/products/product-details/similar-products.tsx
@@ -0,0 +1,246 @@
+import Link from 'next/link'
+import React from 'react'
+
+const SimilarProducts = () => {
+  return (
+    <div className="hidden col-span-12 xxl:col-span-12 xl:col-span-6 lg:col-span-6 md:col-span-6 sm:col-span-12 md:block">
+        <p className="text-[.9375rem] font-semibold mb-2">
+            Similar Products :
+        </p>
+        <div className="table-responsive">
+            <table className="table min-w-full table-bordered whitespace-nowrap">
+                <tbody>
+                    <tr>
+                        <td>
+                            <Link href="#!">
+                                <div className="flex items-start">
+                                    <div className="similar-products-image me-2">
+                                        <img src="../../../../assets/images/ecommerce/png/16.png" alt="" />
+                                    </div>
+                                    <div className="flex-grow">
+                                        <p className="mb-1 text-[0.875rem] font-semibold similar-product-name text-truncate">
+                                            Omega watch series 3
+                                        </p>
+                                        <p className="mb-0">
+                                            <span className="text-white badge bg-success">
+                                                4.4
+                                                <i className="ri-star-s-fill ms-1"></i>
+                                            </span>
+                                            <span className="text-[#8c9097] dark:text-white/50 ms-1">
+                                                (13,256)
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="text-center">
+                                        <p className="mb-0 text-[1rem] font-semibold">
+                                            $1,099
+                                        </p>
+                                        <p className="mb-0 text-[#8c9097] dark:text-white/50">
+                                            <s>$1,759</s>
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </td>
+                    </tr>
+                    <tr className="border border-defaultborder dark:border-defaultborder/10">
+                        <td>
+                            <Link href="#!">
+                                <div className="flex items-start">
+                                    <div className="similar-products-image me-2">
+                                        <img src="../../../../assets/images/ecommerce/png/17.png" alt="" />
+                                    </div>
+                                    <div className="flex-grow">
+                                        <p className="mb-1 text-[0.875rem] font-semibold similar-product-name text-truncate">
+                                            Strap on watch(White)
+                                        </p>
+                                        <p className="mb-0">
+                                            <span className="text-white badge bg-success">
+                                                3.8
+                                                <i className="ri-star-s-fill ms-1"></i>
+                                            </span>
+                                            <span className="text-[#8c9097] dark:text-white/50 ms-1">(5,289)</span>
+                                        </p>
+                                    </div>
+                                    <div className="text-center">
+                                        <p className="mb-0 text-[1rem] font-semibold">
+                                            $7,99
+                                        </p>
+                                        <p className="mb-0 text-[#8c9097] dark:text-white/50">
+                                            <s>$1,299</s>
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </td>
+                    </tr>
+                    <tr className="border border-defaultborder dark:border-defaultborder/10">
+                        <td>
+                            <Link href="#!">
+                                <div className="flex items-start">
+                                    <div className="similar-products-image me-2">
+                                        <img src="../../../../assets/images/ecommerce/png/18.png" alt="" />
+                                    </div>
+                                    <div className="flex-grow">
+                                        <p className="mb-1 font-semibold similar-product-name text-truncate">
+                                            Orange watch series 3
+                                        </p>
+                                        <p className="mb-0">
+                                            <span className="text-white badge bg-success">
+                                                4.1
+                                                <i className="ri-star-s-fill ms-1"></i>
+                                            </span>
+                                            <span className="text-[#8c9097] dark:text-white/50 ms-1">(2,476)</span>
+                                        </p>
+                                    </div>
+                                    <div className="text-center">
+                                        <p className="mb-0 text-[1rem] font-semibold">
+                                            $1,499
+                                        </p>
+                                        <p className="mb-0 text-[#8c9097] dark:text-white/50">
+                                            <s>$2,599</s>
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </td>
+                    </tr>
+                    <tr className="border border-defaultborder dark:border-defaultborder/10">
+                        <td>
+                            <Link href="#!">
+                                <div className="flex items-start">
+                                    <div className="similar-products-image me-2">
+                                        <img src="../../../../assets/images/ecommerce/png/19.png" alt="" />
+                                    </div>
+                                    <div className="flex-grow">
+                                        <p className="mb-1 font-semibold similar-product-name text-truncate">
+                                            Omega watch special edition
+                                        </p>
+                                        <p className="mb-0">
+                                            <span className="text-white badge bg-success">
+                                                3.8
+                                                <i className="ri-star-s-fill ms-1"></i>
+                                            </span>
+                                            <span className="text-[#8c9097] dark:text-white/50 ms-1">(1,893)</span>
+                                        </p>
+                                    </div>
+                                    <div className="text-center">
+                                        <p className="mb-0 text-[1rem] font-semibold">
+                                            $2,299
+                                        </p>
+                                        <p className="mb-0 text-[#8c9097] dark:text-white/50">
+                                            <s>$3,299</s>
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </td>
+                    </tr>
+                    <tr className="border border-defaultborder dark:border-defaultborder/10">
+                        <td>
+                            <Link href="#!">
+                                <div className="flex items-start">
+                                    <div className="similar-products-image me-2">
+                                        <img src="../../../../assets/images/ecommerce/png/20.png" alt="" />
+                                    </div>
+                                    <div className="flex-grow">
+                                        <p className="mb-1 font-semibold similar-product-name text-truncate">
+                                            Fassil smart watch(45mm)
+                                        </p>
+                                        <p className="mb-0">
+                                            <span className="text-white badge bg-success">
+                                                4.5
+                                                <i className="ri-star-s-fill ms-1"></i>
+                                            </span>
+                                            <span className="text-[#8c9097] dark:text-white/50 ms-1">(12,923)</span>
+                                        </p>
+                                    </div>
+                                    <div className="text-center">
+                                        <p className="mb-0 text-[1rem] font-semibold">
+                                            $8,99
+                                        </p>
+                                        <p className="mb-0 text-[#8c9097] dark:text-white/50">
+                                            <s>$1,299</s>
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </td>
+                    </tr>
+                    <tr className="border border-defaultborder dark:border-defaultborder/10">
+                        <td>
+                            <Link href="#!">
+                                <div className="flex items-start">
+                                    <div className="similar-products-image me-2">
+                                        <img src="../../../../assets/images/ecommerce/png/21.png" alt="" />
+                                    </div>
+                                    <div className="flex-grow">
+                                        <p className="mb-1 font-semibold similar-product-name text-truncate">
+                                            Electro viintage watch(40mm)
+                                        </p>
+                                        <p className="mb-0">
+                                            <span className="text-white badge bg-success">
+                                                4.1
+                                                <i className="ri-star-s-fill ms-1"></i>
+                                            </span>
+                                            <span className="text-[#8c9097] dark:text-white/50 ms-1">(453)</span>
+                                        </p>
+                                    </div>
+                                    <div className="text-center">
+                                        <p className="mb-0 text-[1rem] font-semibold">
+                                            $1,799
+                                        </p>
+                                        <p className="mb-0 text-[#8c9097] dark:text-white/50">
+                                            <s>$2,499</s>
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </td>
+                    </tr>
+                    <tr className="border border-defaultborder dark:border-defaultborder/10">
+                        <td>
+                            <Link href="#!">
+                                <div className="flex items-start">
+                                    <div className="similar-products-image me-2">
+                                        <img src="../../../../assets/images/ecommerce/png/22.png" alt="" />
+                                    </div>
+                                    <div className="flex-grow">
+                                        <p className="mb-1 font-semibold similar-product-name text-truncate">
+                                            Bolts smart watch(Amoled)
+                                        </p>
+                                        <p className="mb-0">
+                                            <span className="text-white badge bg-success">
+                                                4.3
+                                                <i className="ri-star-s-fill ms-1"></i>
+                                            </span>
+                                            <span className="text-[#8c9097] dark:text-white/50 ms-1">(1,874)</span>
+                                        </p>
+                                    </div>
+                                    <div className="text-center">
+                                        <p className="mb-0 text-[1rem] font-semibold">
+                                            $2,299
+                                        </p>
+                                        <p className="mb-0 text-[#8c9097] dark:text-white/50">
+                                            <s>$2,999</s>
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="grid">
+                            <button type="button" className="ti-btn ti-btn-primary !font-medium">
+                                View All Products
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default SimilarProducts
