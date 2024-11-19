ed206ca3706ce84ad881eb0a3d16066f30b59dd3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-19 07:32:04 +0200

    Added ButtonGroup

---
+++ components/products/product-details/button-group.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+
+const ButtonGroup = () => {
+  return (
+    <div className="grid gap-2">
+        <Link href="/components/pages/ecommerce/cart/" 
+            className="ti-btn bg-primary !font-medium text-white">
+            Add To Cart
+        </Link>
+        <Link href="/components/pages/ecommerce/checkout/" 
+            className="ti-btn bg-success !font-medium text-white">
+            Buy Now
+        </Link>
+        <Link href="/components/pages/ecommerce/wishlist/" 
+            className="ti-btn bg-light !font-medium">
+            Add To Wishlist
+        </Link>
+    </div>
+  )
+}
+
+export default ButtonGroup
