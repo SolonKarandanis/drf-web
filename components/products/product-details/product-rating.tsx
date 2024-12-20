7ef28807ceafb81476665caf7a01d09d75622e7b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-12-20 07:27:45 +0200

    Implementing product rating

--- components/products/product-details/product-rating.tsx
+++ components/products/product-details/product-rating.tsx
@@ -11,15 +11,31 @@ const ProductRating:FC<Props> = ({
 }) => {
   return (
     <p className="text-[1.125rem] mb-4">
-        <i className="align-middle ri-star-s-fill text-warning"></i>
-        <i className="align-middle ri-star-s-fill text-warning"></i>
-        <i className="align-middle ri-star-s-fill text-warning"></i>
-        <i className="align-middle ri-star-s-fill text-warning"></i>
-        <i className="align-middle ri-star-half-s-fill text-warning"></i>
-        <span className="font-semibold text-[#8c9097] dark:text-white/50 ms-1">
-            4.3
-            <span className="text-info">(2.4k Reviews)</span>
-        </span>
+      {(!reviews || reviews ===0) &&(
+        <>
+          <i className="align-middle ri-star-line text-warning"></i>
+          <i className="align-middle ri-star-line text-warning"></i>
+          <i className="align-middle ri-star-line text-warning"></i>
+          <i className="align-middle ri-star-line text-warning"></i>
+          <i className="align-middle ri-star-line text-warning"></i>
+          <span className="font-semibold text-[#8c9097] dark:text-white/50 ms-1">
+              <span className="text-info">(0 Reviews)</span>
+          </span>
+        </>
+      )}
+      {reviews && reviews>0 &&(
+        <>
+          <i className="align-middle ri-star-s-fill text-warning"></i>
+          <i className="align-middle ri-star-s-fill text-warning"></i>
+          <i className="align-middle ri-star-s-fill text-warning"></i>
+          <i className="align-middle ri-star-s-fill text-warning"></i>
+          <i className="align-middle ri-star-half-s-fill text-warning"></i>
+          <span className="font-semibold text-[#8c9097] dark:text-white/50 ms-1">
+              {rating}
+              <span className="text-info">({reviews} Reviews)</span>
+          </span>
+        </>
+      )}
     </p>
   )
 }
