ffdc1f010f99f2ca7234970638a9ea6402fdee57
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-01-04 14:40:27 +0200

    Implementing Product schema

--- schemas/product.schemas.ts
+++ schemas/product.schemas.ts
@@ -1,4 +1,34 @@
 import * as z from "zod";
 import { type TranslationValues } from "next-intl";
 
-type Messages = keyof IntlMessages["USERS"]["VALIDATION"];
+type Messages = keyof IntlMessages["PRODUCTS"]["VALIDATION"];
+
+export function getSaveProductSchema(
+    t?: (key: Messages, object?: TranslationValues | undefined) => string
+){
+    return z.object({
+        name: z.string().min(1,{
+            message: t?.("required-name"),
+        }),
+        sku: z.string().email({
+            message: t?.("required-sku"),
+        }),
+        category:z.number({
+            message: t?.("required-category"),
+        }),
+        gender:z.number({
+            message: t?.("required-gender"),
+        }),
+        brand:z.number({
+            message: t?.("required-brand"),
+        }),
+        sizes:z.array(z.number()).min(1,{
+            message: t?.("required-sizes"),
+        }),
+        colors:z.array(z.number()).min(1,{
+            message: t?.("required-colors"),
+        }),
+    });
+} 
+
+export type SaveProductSchema = z.infer<ReturnType<typeof getSaveProductSchema>>;
