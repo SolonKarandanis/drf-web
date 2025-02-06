14d7ad27ca6f0cb697aeba590435bf0f9f949b58
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-02-06 11:34:25 +0200

    Created CartDetailsSection component

--- app/[locale]/(protected)/cart/page.tsx
+++ app/[locale]/(protected)/cart/page.tsx
@@ -1,4 +1,5 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/options'
+import CartDetailsSection from '@/components/cart/cart-details-section'
 import CartItemsSection from '@/components/cart/cart-items-section'
 import { CartItem } from '@/models/cart.models'
 import PageHeader from '@/shared/layout-components/page-header/PageHeader'
@@ -83,38 +84,17 @@ const CartPage = async () => {
                                         aria-describedby="coupons" />
                                     <button 
                                         type="button" 
-                                        className="ti-btn !bg-primary !text-white !font-medium !rounded-s-none !mb-0" 
+                                        className="ti-btn !bg-light !text-white !font-medium !rounded-s-none !mb-0" 
                                         id="coupons">
                                         Apply
                                     </button>
                                 </div>
                                 <Link href="#!" className="text-[0.75rem] text-success">10% off on first purchase</Link>
                             </div>
-                            <div className="p-4 border-b border-dashed dark:border-defaultborder/10">
-                                <div className="flex items-center justify-between mb-4">
-                                    <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Sub Total</div>
-                                    <div className="font-semibold text-[0.875rem]">$1,299</div>
-                                </div>
-                                <div className="flex items-center justify-between mb-4">
-                                    <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Discount</div>
-                                    <div className="font-semibold text-[0.875rem] text-success">10% - $129</div>
-                                </div>
-                                <div className="flex items-center justify-between mb-4">
-                                    <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Delivery Charges</div>
-                                    <div className="font-semibold text-[0.875rem] text-danger">- $49</div>
-                                </div>
-                                <div className="flex items-center justify-between mb-4">
-                                    <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Service Tax (18%)</div>
-                                    <div className="font-semibold text-[0.875rem]">- $169</div>
-                                </div>
-                                <div className="flex items-center justify-between">
-                                    <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Total :</div>
-                                    <div className="font-semibold text-[0.875rem] text-primary"> $1,387</div>
-                                </div>
-                            </div>
+                            <CartDetailsSection />
                             <div className="grid p-4">
                                 <Link href="/components/pages/ecommerce/checkout/" className="ti-btn bg-success  text-white !font-medium !mb-2">Proceed To Checkout</Link>
-                                <Link href="/components/pages/ecommerce/products/" className="ti-btn bg-light  !font-medium">Countinue Shopping</Link>
+                                <Link href="/products/search" className="ti-btn bg-light  !font-medium">Countinue Shopping</Link>
                             </div>
                         </div>
                     </div>
