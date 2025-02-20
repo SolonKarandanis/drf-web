e446e1be4d9eaaaad3d6b3bb8c6f5ba46dabe66c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-02-20 07:34:51 +0200

    Implemented clear cart

--- components/cart/buttons-section.tsx
+++ components/cart/buttons-section.tsx
@@ -37,17 +37,17 @@ const ButtonSection = () => {
                 className="ti-btn !font-medium text-white"
                 isLoading={mutationLoading}
                 disabled={mutationLoading}
-                >
+                onClick={handleClearCart}>
                 {t(`clear-cart`)}
             </FormButton>
             <Link 
                 href="/components/pages/ecommerce/checkout/" 
-                className="ti-btn bg-success hover:bg-emerald-500 dark:bg-emerald-600 dark:hover:bg-emerald-700 text-white !font-medium !mb-2">
+                className="ti-btn bg-success hover:bg-emerald-600 dark:bg-emerald-600 dark:hover:bg-emerald-700 text-white !font-medium !mb-2">
                     {t("proceed-to-checkout")}
             </Link>
             <Link 
                 href="/products/search" 
-                className="ti-btn bg-light  !font-medium">
+                className="ti-btn bg-light hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-700  !font-medium">
                     {t("continue-shopping")}
             </Link>
         </div>
