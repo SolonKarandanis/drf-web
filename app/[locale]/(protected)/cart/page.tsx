de9fa5f91141630ce9a0dd3e80ccf81218cd6830
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-02-24 08:04:31 +0200

    Created Cart component

--- app/[locale]/(protected)/cart/page.tsx
+++ app/[locale]/(protected)/cart/page.tsx
@@ -1,9 +1,5 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/options'
-import ButtonSection from '@/components/cart/buttons-section'
-import CartDetailsSection from '@/components/cart/cart-details-section'
-import CartItemsSection from '@/components/cart/cart-items-section'
-import CouponSection from '@/components/cart/coupon-section'
-import DeliverySection from '@/components/cart/delivery-section'
+import Cart from '@/components/cart/cart'
 import PageHeader from '@/shared/layout-components/page-header/PageHeader'
 import { Metadata } from 'next'
 import { getServerSession } from 'next-auth'
@@ -32,24 +28,7 @@ const CartPage = async () => {
                 currentpage="Cart" 
                 activepage="Cart" 
                 mainpage={fullname} />
-            <div className="grid grid-cols-12 gap-0 sm:gap-x-6">
-               <CartItemsSection />
-                <div className="col-span-12 xxl:col-span-3">
-                    <div className="box">
-                        <div className="block p-4 border-b dark:border-defaultborder/10">
-                            <div className="text-center alert alert-primary" role="alert">
-                                <span className="text-defaulttextcolor">Sale Ends in</span> <span className="font-semibold text-[0.875rem] text-primary ms-1">8hours:32minutes</span>
-                            </div>
-                        </div>
-                        <div className="box-body !p-0">
-                            <DeliverySection />
-                            <CouponSection />
-                            <CartDetailsSection />
-                            <ButtonSection />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Cart />
         </>
     )
 }
---
+++ components/cart/cart.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import ButtonSection from "./buttons-section";
+import CartDetailsSection from "./cart-details-section";
+import CartItemsSection from "./cart-items-section";
+import CouponSection from "./coupon-section";
+import DeliverySection from "./delivery-section";
+
+const Cart = () => {
+  return (
+    <div className="grid grid-cols-12 gap-0 sm:gap-x-6">
+        <CartItemsSection />
+        <div className="col-span-12 xxl:col-span-3">
+            <div className="box">
+                <div className="block p-4 border-b dark:border-defaultborder/10">
+                    <div className="text-center alert alert-primary" role="alert">
+                        <span className="text-defaulttextcolor">Sale Ends in</span> <span className="font-semibold text-[0.875rem] text-primary ms-1">8hours:32minutes</span>
+                    </div>
+                </div>
+                <div className="box-body !p-0">
+                    <DeliverySection />
+                    <CouponSection />
+                    <CartDetailsSection />
+                    <ButtonSection />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
