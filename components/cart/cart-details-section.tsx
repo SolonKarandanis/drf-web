35539f593c53fb6314d974534204798b7cca5265
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-02-25 07:36:42 +0200

    Added translations

--- components/cart/cart-details-section.tsx
+++ components/cart/cart-details-section.tsx
@@ -2,33 +2,45 @@
 
 import CurrencyFormatter from "@/shared/components/currency-formatter/currency-formatter";
 import { useCartData } from "./providers/cart-context";
+import { useTranslations } from "next-intl";
 
 const CartDetailsSection = () => {
+    const t = useTranslations("CART.LABELS");
     const {
         totalCartValue
     }= useCartData();
     return (
         <div className="p-4 border-b border-dashed dark:border-defaultborder/10">
             <div className="flex items-center justify-between mb-4">
-                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Sub Total</div>
+                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">
+                    {t("sub-total")}
+                </div>
                 <div className="font-semibold text-[0.875rem]">
                     <CurrencyFormatter amount={totalCartValue} />
                 </div>
             </div>
             <div className="flex items-center justify-between mb-4">
-                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Discount</div>
+                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">
+                    {t("discount")}
+                </div>
                 <div className="font-semibold text-[0.875rem] text-success">10% - $129</div>
             </div>
             <div className="flex items-center justify-between mb-4">
-                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Delivery Charges</div>
+                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">
+                    {t("delivery-charges")}
+                </div>
                 <div className="font-semibold text-[0.875rem] text-danger">- $49</div>
             </div>
             <div className="flex items-center justify-between mb-4">
-                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Service Tax (18%)</div>
+                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">
+                    {t("service-tax")} (18%)
+                </div>
                 <div className="font-semibold text-[0.875rem]">- $169</div>
             </div>
             <div className="flex items-center justify-between">
-                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">Total :</div>
+                <div className="text-[#8c9097] dark:text-white/50 opacity-[0.7]">
+                    {t("total")} :
+                </div>
                 <div className="font-semibold text-[0.875rem] text-sky-600">
                     <CurrencyFormatter amount={totalCartValue} />
                 </div>
